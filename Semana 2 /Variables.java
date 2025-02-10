c1cef4cbc6391de40364e280c033c3822a232350
NeitanNarvaez <neitan.narvaez@campusucc.edu.co>
2025-02-10 14:06:27 +0000

    Condicionales

---
+++ Semana 2 /Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Declaracion de una Variables de tipo entero
+        int edad = 25;
+        //Declaracion de una variable de tipo real 
+        double altura = 1.75;
+        //Declaracion de una variable de cadena    
+        String nombre = "Juan";
+        //Declaracion de una variable de tipo boolean
+        Boolean esEstudiante = true;
+        /*
+        Con la intruccion System.out.println se Mostrara las variables y una de ellas estara concatenada con el signo (+) y la leyenda 
+        */
+        System.out.println("nombre : " + nombre + ", edad : "+ edad +", altura: "+ altura);
+        System.out.println("Estudiante "+ esEstudiante);
+
+    }
+}
