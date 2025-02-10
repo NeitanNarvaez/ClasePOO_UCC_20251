c0d72dd531cc18cd2876c2a26ff7085bf0d6f084
NeitanNarvaez <neitan.narvaez@campusucc.edu.co>
2025-02-10 15:02:10 +0000

    Bucles

---
+++ Semana 2 /Bucles.java
@@ -0,0 +1,17 @@
+public class Bucles {
+    public static void main ( String [] args ) {
+    for (int i = 1; i <= 5; i ++) {
+    System . out . println (" POO : " + i ) ;
+    }
+}
+{
+    System.out.println("----------------------------------------------------------------");
+    
+    int a = 1;
+    while( a <= 5) {
+    System.out.println (" P.O.O : " + a ) ;
+    a++;
+    }
+}
+}
+    
