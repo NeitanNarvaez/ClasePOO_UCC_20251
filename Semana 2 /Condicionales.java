176b58cddb73d0560c83a78795d7c85be0efdaca
NeitanNarvaez <neitan.narvaez@campusucc.edu.co>
2025-02-10 14:06:51 +0000

    Variables

---
+++ Semana 2 /Condicionales.java
@@ -0,0 +1,14 @@
+public class Condicionales {
+public static void main ( String [] args ) {
+    // Condicionales 
+    
+    int numero = -5;
+    if ( numero > 0) {
+        System . out . println ("El numero es positivo ") ;
+    } else if ( numero < 0) {
+        System . out . println ("El numero es negativo ") ;
+    } else {
+        System . out . println ("El numero es cero ") ;
+    }
+}
+}
